0cc69c167398ff741c0dab0ce092a224ce97af86
Samomidiji <omidijisamuel96@gmail.com>
2021-09-02 09:45:38 -0500

    update

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
   background(222);
-  image(bulbs, width/2, height/2);
-  image(lights, width/2, height/2 - 100);
-  image(flower, width/2 + 100, height/2);
+  image(bulbs, width/2, height/2, 547, 364);
+  image(lights, width/2, height/2 - 100, 600, 400);
+  image(flower, width/2 + 100, height/2, 306, 459);
 }
