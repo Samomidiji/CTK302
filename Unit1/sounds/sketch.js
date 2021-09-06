f52199a980d6c8083cd7f9fdce5b3882001129e8
Samomidiji <omidijisamuel96@gmail.com>
2021-09-06 17:34:22 -0500

    This is sound

---
+++ Unit1/sounds/sketch.js
@@ -0,0 +1,28 @@
+let sound1;
+let sound2;
+let sound3;
+
+
+function setup() {
+  createCanvas(500, 500);
+  background(20,233,20);
+  sound1 = loadSound("assest/ForKitchen.mp3");
+  sound2 = loadSound("assest/SoftAbstraction.mp3");
+  sound3 = loadSound("assest/UpbeatMusic.mp3");
+  textAlign(CENTER);
+}
+
+function draw() {
+  text("Click her to play", width/2, height/2);
+
+}
+
+function mousePressed(){
+  if (sound1.isPlaying()){
+    background(20,233,20);
+    sound1.stop();
+  } else {
+    background(255,54,20);
+    sound1.play();
+  }
+}
