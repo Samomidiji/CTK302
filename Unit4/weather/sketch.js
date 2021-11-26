d2bc1595ac6bb74b298649aa22fe683cbe000968
Samomidiji <omidijisamuel96@gmail.com>
2021-11-25 22:46:47 -0600

    Weather

--- Unit4/weather/sketch.js
+++ Unit4/weather/sketch.js
@@ -7,9 +7,18 @@ var x = 0;
 var windspeed = 0;
 let humidity = 0;
 let temp = 0;
+let bg;
+let font;
+let fontB;
+let loct;
+let cloud;
+let wind;
+let drop;
+let dots = [];
+let maxDots = 500;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 
   // HERE is the call to get the weather.
 
@@ -22,6 +31,19 @@ function setup() {
   var myBigString = myCityString + myIDString;
 
   loadJSON(myBigString, gotData); // that gotData function happens when JSON comes back.
+  textAlign(CENTER);
+  imageMode(CENTER);
+  bg = loadImage('asset/bg.jpg');
+  font = loadFont('asset/GilroyR.otf')
+  fontB = loadFont('asset/GilroySB.otf')
+  loct = loadImage('asset/loct.png');
+  cloud = loadImage('asset/cloud.png');
+  wind = loadImage('asset/wind.png');
+  drop = loadImage('asset/drop.png');
+
+  for (let i = 0; i <= maxDots; i++) {
+    dots.push(new Dot());
+  }
 
 }
 
@@ -40,6 +62,8 @@ function gotData(data) {
 
 
 function draw() {
+  image(bg, width / 2, height / 2, windowWidth, windowHeight);
+
   switch (myState) {
     case 0:
       if (weather) {
@@ -48,25 +72,64 @@ function draw() {
       break;
 
     case 1:
-      background(200);
-      fill('black');
-      text("What is the weather in " + weather.name + "?", 20, 20);
-      text("windspeed is " + windspeed, 20, 40);
-      text("temperature is " + temp, 20, 60);
-      text("humidity is " + humidity, 20, 80);
-      text("Description is " + desc, 20, 100);
+      // background(200);
+
+      image(loct, width / 2 - 65, height / 2 - 367, 240 / 20, 368 / 20);
+      fill('White');
+      textFont(fontB);
+      textSize(30);
+      text(weather.name, width / 2, 145);
+
+      textFont(font);
+      textSize(80);
+      text(temp + "°F", width / 2, 250);
+
+      image(cloud, width / 2, height / 2 - 30, 683 / 1.5, 576 / 1.5);
+
+      image(wind, width / 2 - 165, height / 2 + 233, 235 / 10, 235 / 10);
+      image(drop, width / 2 + 40, height / 2 + 233, 221 / 10, 217 / 10);
+      textFont(font);
+      textSize(20);
+      text("Windspeed: " + windspeed, width / 2 - 68, 740);
+      text("Humidity: " + humidity, width / 2 + 120, 740);
+
+      textFont(fontB);
+      textSize(20);
+      text("Description is " + desc, width / 2, 820);
+
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].display();
+        dots[i].move();
+}
+        // cloud
+        // fill('white');
+        // noStroke();
+        // ellipse(x, 300, 200, 100);
+        //
+        // // move the cloud's x position
+        // x = x + windspeed;
+        // if (x > width) x = 0;
+
+        break;
 
 
-      // cloud
-      fill('white');
-      noStroke();
-      ellipse(x, 300, 200, 100);
+  }
+}
 
-      // move the cloud's x position
-      x = x + windspeed;
-      if (x > width) x = 0;
+class Dot {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.a = 40
+  }
 
-      break;
+  display() {
+    noStroke();
+    fill(255,255,255, this.a);
+    ellipse(this.pos.x, this.pos.y, 5, 5);
+  }
 
+  move() {
+    this.pos.x = this.pos.x + windspeed;
+    if (this.pos.x > width) this.pos.x = 0;
   }
 }
