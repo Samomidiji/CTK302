910b9e5e30b03f1c8dd855af12079008f692065b
Samomidiji <omidijisamuel96@gmail.com>
2021-12-02 12:23:30 -0600

    LAST BEFORE A PAUSE

--- Unit4/SpaceShooter2/sketch.js
+++ Unit4/SpaceShooter2/sketch.js
@@ -258,10 +258,10 @@ function game() {
 
   image(ship, cx, cy, 70, 109);
 
-  if (cx >= width) cx = width;
-  if (cx <= 0) cx = 0;
-  if (cy >= height) cy = height;
-  if (cy <= 0) cy = 0;
+  // if (cx >= width) cx = width;
+  // if (cx <= 0) cx = 0;
+  // if (cy >= height) cy = height;
+  // if (cy <= 0) cy = 0;
   checkForKey();
 
   //  Touch screen to shoot
